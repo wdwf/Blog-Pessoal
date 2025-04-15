3b59b2aafe92034130e7b8815d45a910e0625c27
wdwf <w_dwf@hotmail.com>
2025-04-14 23:44:37 -0300

    :white_check_mark: feat: Implementa método para buscar postagem por ID na PostagemController

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -3,9 +3,11 @@ package com.generation.blogpessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,13 @@ public class PostagemController {
   @GetMapping
   public ResponseEntity<List<Postagem>> getAll() {
     return ResponseEntity.ok(postagemRepository.findAll());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Postagem> getById(@PathVariable Long id) {
+    return postagemRepository.findById(id)
+        .map(resposta -> ResponseEntity.ok(resposta))
+        .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
   }
 
 }
