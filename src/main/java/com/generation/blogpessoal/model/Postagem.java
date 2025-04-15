1d292ed76f1d504b956b35a2becd8cd1c0df3a2d
wdwf <w_dwf@hotmail.com>
2025-04-14 23:22:55 -0300

    :construction: feat: Cria a entidade postagem

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,59 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  
+  @NotBlank(message = "O atributo é necessario") //não permite espaços em branco
+  @Size(min = 3, max = 255)
+  private String titulo;
+
+  @NotBlank(message = "O atributo é necessario") //não permite espaços em branco
+  @Size(min = 3, max = 1000, message = "O atributo deve conter no minimo 10 e no maximo 1000 caracteres")
+  private String texto;
+
+  @UpdateTimestamp
+  private LocalDate data;
+
+  public Long getId() {
+    return id;
+  }
+  public void setId(Long id) {
+    this.id = id;
+  }
+  public String getTitulo() {
+    return titulo;
+  }
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+  public String getTexto() {
+    return texto;
+  }
+  public void setTexto(String texto) {
+    this.texto = texto;
+  }
+  public LocalDate getData() {
+    return data;
+  }
+  public void setData(LocalDate data) {
+    this.data = data;
+  }
+  
+}
