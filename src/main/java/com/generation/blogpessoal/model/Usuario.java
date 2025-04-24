9255780c0191f74d3e2ab825f01134ab52d5bb60
wdwf <w_dwf@hotmail.com>
2025-04-23 23:19:37 -0300

    :bug: fix: Corrige a tabela de usuários e validações no modelo Usuario

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -12,11 +12,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "tb_usuario")
+@Table(name = "tb_usuarios")
 public class Usuario {
 
     @Id
@@ -27,16 +29,66 @@ public class Usuario {
     private String nome;
 
     @NotBlank(message = "O Atributo Usuario é obrigatório")
+    @Email(message = "O Atributo Usuário deve ser um email válido!")
     private String usuario;
 
     @NotNull(message = "O Atributo Senha é obrigatório")
+    @Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
     private String senha;
 
-    @NotBlank(message = "O Atributo Foto é obrigatório")
+    @Size(max = 5000, message = "O link da foto não pode ser maior do que 5000 caracteres")
     private String foto;
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = { CascadeType.REMOVE })
     @JsonIgnoreProperties("usuario")
     private List<Postagem> postagem;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public List<Postagem> getPostagem() {
+        return postagem;
+    }
+
+    public void setPostagem(List<Postagem> postagem) {
+        this.postagem = postagem;
+    }
 
 }
